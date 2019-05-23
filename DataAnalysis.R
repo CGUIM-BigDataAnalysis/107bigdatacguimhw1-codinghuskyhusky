072010f46e2a1c60cbad72bbc9eec864b8874a19
codinghuskyhusky <43308589+codinghuskyhusky@users.noreply.github.com>
2019-05-23 19:33:23 +0800

    push homework

--- DataAnalysis.R
+++ DataAnalysis.R
@@ -24,3 +24,21 @@ for(i in 1:length(job)){
   spjob[i]<- job[[i]][1]}
 
 table(spjob)
+
+X103salary <- read_csv("C:/Users/kazusa/Desktop/work/103worksalary.csv")
+X103salary <- read_csv("C:/Users/kazusa/Desktop/work/104worksalary.csv")
+X105salary <- read_csv("C:/Users/kazusa/Desktop/work/105worksalary.csv")
+X106salary <- read_csv("C:/Users/kazusa/Desktop/work/106worksalary.csv")
+
+X103salary$`專業人員-薪資`<-as.numeric(X103salary$`專業人員-薪資`)
+X104salary$`專業人員-薪資`<-as.numeric(X104salary$`專業人員-薪資`)
+X105salary$`專業人員-薪資`<-as.numeric(X105salary$`專業人員-薪資`)
+X106salary$`專業人員-薪資`<-as.numeric(X106salary$`專業人員-薪資`)
+
+X103salary$年度<-as.numeric(X103salary$年度)
+X104salary$年度<-as.numeric(X104salary$年度)
+X105salary$年度<-as.numeric(X105salary$年度)
+X106salary$年度<-as.numeric(X106salary$年度)
+
+rbind(X103salary,X103salary,X105salary,X106salary)
+
